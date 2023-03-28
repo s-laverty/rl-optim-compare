5ecfd9050d9166b66849bfa9b4821fa4b5ee0947
Steven Laverty <sdlaverty123@gmail.com>
2023-03-28 14:28:40 -0400

    Implemented training pipeline

--- train.py
+++ train.py
@@ -8,18 +8,22 @@ Created on 3/17/2023 by Steven Laverty (lavers@rpi.edu).
 
 import argparse
 import enum
+import itertools
 import json
 import logging
+import multiprocessing as mp
 import os
 import pathlib
+import random
+import time
 import typing
 from collections import deque
 
 import gymnasium as gym
-import numpy as np
 import torch
+import torch.nn as nn
 import torch.optim as optim
-import torch.multiprocessing as mp
+from torch_wrapper import TorchWrapper
 
 from project.net import QNet
 
@@ -29,14 +33,19 @@ logging.basicConfig(
 logger = logging.getLogger(__file__)
 logger.setLevel(logging.INFO)
 
+OBS_SHAPE = 4
+NUM_ACTIONS = 2
 
-class Optimizer(str, enum.Enum):
+
+class OptimType(str, enum.Enum):
     RMS_PROP = 'rms_prop'
     ADAM = 'adam'
     R_ADAM = 'r_adam'
+    ADAM_W = 'adam_w'
 
 
 class OptimConfig(typing.TypedDict, total=False):
+    optim_type: OptimType
     lr: float
     eps: float
     weight_decay: float
@@ -57,26 +66,28 @@ class Config(typing.TypedDict):
     q_epsilon_min: float
     q_epsilon_decay: float
     q_replay_buf_len: int
-    batch_size: int
+    q_target_update_tau: float
     train_steps: int
-    train_steps_target: int
+    batch_size: int
     num_eval: int
-    optimizers: dict[Optimizer, OptimConfig]
+    checkpoint_steps: int
+    optimizers: list[OptimConfig]
 
 
 class Experience(typing.NamedTuple):
     state: torch.Tensor
     action: int
     reward: float
-    next_state: torch.Tensor
+    next_state: torch.Tensor | None
 
 
 class Checkpoint(typing.NamedTuple):
-    net_state_dict: dict
+    num_steps: int
+    policy_net_state_dict: dict
+    target_net_state_dict: dict
     optimizer_state_dict: dict
     replay_buffer: deque[Experience]
-    train_reward_hist: typing.Sequence[float]
-    eval_reward_hist: typing.Sequence[float]
+    eval_hist: list[float]
 
 
 def save_checkpoint(
@@ -92,8 +103,9 @@ def save_checkpoint(
 
 def load_checkpoint(
     file: str | os.PathLike | typing.BinaryIO | typing.IO[bytes],
+    device: torch.device = 'cpu',
 ) -> Checkpoint:
-    return torch.load(file, map_location='cpu')
+    return torch.load(file, map_location=device)
 
 
 def checkpoint_path(config: Config, iteration: int) -> pathlib.Path:
@@ -101,10 +113,14 @@ def checkpoint_path(config: Config, iteration: int) -> pathlib.Path:
     return pathlib.Path(config['checkpoint_dir']).joinpath(name)
 
 
+def checkpoint_name(rank: int, optim_rank: int) -> pathlib.Path:
+    return pathlib.Path('{}_{}.pt'.format(rank, optim_rank))
+
+
 def latest_checkpoint_iteration(config: Config) -> int:
     iteration = -1
     glob_pattern = 'checkpoint_{}_iter_*.pt'.format(config['name'])
-    num_checkpoints = len(config['optimizers']) * config['num_models']
+    num_checkpoints = config['num_models'] * len(config['optimizers'])
     for checkpoint_path in pathlib.Path(config['checkpoint_dir']).iterdir():
         if (
             checkpoint_path.match(glob_pattern)
@@ -117,25 +133,14 @@ def latest_checkpoint_iteration(config: Config) -> int:
     return iteration
 
 
-def train_net(
-    config: Config,
-    optimizer: Optimizer,
-    rank: int,
-    seed: int,
-    iteration: int,
-    num_iter: int = 1,
-) -> None:
-    pass
-
 def init_optim(
-    optimizer: Optimizer,
     config: OptimConfig,
-    params: typing.Iterable[torch.nn.Parameter],
+    net: QNet,
 ) -> optim.Optimizer:
-    match optimizer:
+    match config['optim_type']:
         case [RMS_PROP]:
             return optim.RMSprop(
-                params,
+                net.parameters(),
                 config.get('lr'),
                 config.get('rms_alpha'),
                 config.get('eps'),
@@ -145,7 +150,7 @@ def init_optim(
             )
         case [ADAM]:
             return optim.Adam(
-                params,
+                net.parameters(),
                 config.get('lr'),
                 config.get('adam_betas'),
                 config.get('eps'),
@@ -154,14 +159,246 @@ def init_optim(
             )
         case [R_ADAM]:
             return optim.RAdam(
-                params,
+                net.parameters(),
                 config.get('lr'),
                 config.get('adam_betas'),
                 config.get('eps'),
                 config.get('weight_decay'),
             )
         case _:
-            raise ValueError('Optimizer type {} not recognized'.format(optimizer))
+            raise ValueError(
+                'Optimizer type {} not recognized'.format(config['optim_type']))
+
+
+def epsilon_greedy(
+    config: Config,
+    steps: int,
+    net: QNet,
+    obs: torch.Tensor,
+) -> int:
+    epsilon = config['q_epsilon_min'] + (
+        config['q_epsilon_decay']**steps
+        * (config['q_epsilon_max'] - config['q_epsilon_min'])
+    )
+    if random.random() < epsilon:
+        return random.choice(range(NUM_ACTIONS))
+    net.eval()
+    with torch.no_grad():
+        q_values = net(obs)
+    return torch.argmax(q_values)
+
+
+def train_net(
+    config: Config,
+    policy_net: QNet,
+    target_net: QNet,
+    optimizer: optim.Optimizer,
+    replay_buffer: deque[Experience],
+    device: torch.device | None = None,
+) -> None:
+    policy_net.train()
+    target_net.eval()
+
+    # Get batch for training from replay buffer
+    states, actions, rewards, next_states = zip(*random.sample(
+        replay_buffer,
+        config['batch_size'],
+    )),
+    states = torch.stack(states)
+    if device is not None:
+        states = states.to(device)
+    actions = states.new_tensor(actions, torch.long).unsqueeze(0)
+    rewards = states.new_tensor(rewards)
+    next_states_mask = states.new_tensor(
+        tuple(s is not None for s in next_states),
+        torch.bool,
+    )
+    next_states = torch.stack(tuple(s for s in next_states if s is not None))
+    if device is not None:
+        next_states = next_states.to(device)
+
+    # Backpropagation
+    pred_q = policy_net(states).gather(1, actions).squeeze(1)
+    next_v = torch.zeros_like(pred_q)
+    with torch.no_grad():
+        next_v[next_states_mask] = target_net(next_states).max(1)[0]
+    target_q = next_v * config['q_gamma'] + rewards
+    criterion = nn.SmoothL1Loss()
+    loss = criterion(pred_q, target_q)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    # Soft update target net parameters
+    target_net_state_dict = target_net.state_dict()
+    policy_net_state_dict = policy_net.state_dict()
+    for key in target_net_state_dict:
+        target_net_state_dict[key] += config['q_target_update_tau'] * (
+            policy_net_state_dict[key] - target_net_state_dict[key]
+        )
+
+
+def eval_net(
+    config: Config,
+    net: QNet,
+    env: gym.Env,
+) -> float:
+    net.eval()
+    with torch.no_grad():
+        total_rewards = []
+        for _ in range(config['num_eval']):
+            obs, _ = env.reset()
+            total_reward = 0
+            is_term = is_trunc = False
+            while not is_term and not is_trunc:
+                action = torch.argmax(net(obs)).item()
+                obs, reward, is_term, is_trunc, _ = env.step(action)
+                total_reward += reward
+            total_rewards.append(total_reward)
+        return sum(total_rewards) / len(total_rewards)
+
+
+def deep_q(
+    config: Config,
+    rank: int,
+    optim_rank: int,
+    start_iteration: int,
+    device: torch.device,
+    env_seed: int,
+    num_iter: int = 1,
+    *,
+    map_replay_buffer: bool = True,
+    mp_barrier: mp.Barrier | None = None,
+) -> None:
+    name = checkpoint_name(rank, optim_rank)
+    # Load previous checkpoint
+    (
+        num_steps,
+        policy_net_state_dict,
+        target_net_state_dict,
+        optimizer_state_dict,
+        replay_buffer,
+        eval_hist,
+    ) = load_checkpoint(
+        checkpoint_path(config, start_iteration - 1).joinpath(name),
+        device if map_replay_buffer else 'cpu',
+    )
+
+    # Initialize neural nets
+    policy_net = QNet(OBS_SHAPE, NUM_ACTIONS)
+    target_net = QNet(OBS_SHAPE, NUM_ACTIONS)
+    policy_net.load_state_dict(policy_net_state_dict)
+    target_net.load_state_dict(target_net_state_dict)
+    if not map_replay_buffer:
+        policy_net.to(device)
+        target_net.to(device)
+    optimizer = init_optim(config['optimizers'][optim_rank], policy_net)
+    optimizer.load_state_dict(optimizer_state_dict)
+
+    # Initialize environment
+    train_env = TorchWrapper(
+        gym.make('CartPole-v1'),
+        device=device if map_replay_buffer else 'cpu',
+    )
+    eval_env = TorchWrapper(
+        gym.make('CartPole-v1'),
+        device=device,
+        batch_dim=True,
+    )
+
+    # Set training counter (neural net update very config['train_steps'])
+    train_step_counter = num_steps % config['train_steps']
+    if train_step_counter == 0:
+        train_step_counter = config['train_steps']
+
+    # Train for num_iter checkpoint iterations
+    obs, _ = train_env.reset(seed=env_seed)
+    for checkpoint_iteration in range(
+        start_iteration,
+        start_iteration + num_iter,
+    ):
+        for _ in range(config['checkpoint_steps']):
+            # Take an epsilon-greedy action, append to replay buffer
+            action = epsilon_greedy(config, num_steps, policy_net, obs)
+            next_obs, reward, is_term, is_trunc, _ = train_env.step(action)
+            replay_buffer.append(Experience(
+                obs,
+                action,
+                reward,
+                next_obs if not is_term else None,
+            ))
+            obs = next_obs
+            if is_term or is_trunc:
+                obs, _ = train_env.reset()
+            num_steps += 1
+
+            # Update policy (and target) every config['train_steps']
+            train_step_counter -= 1
+            if train_step_counter == 0:
+                train_step_counter = config['train_steps']
+                train_net(
+                    config,
+                    policy_net,
+                    target_net,
+                    optimizer,
+                    replay_buffer,
+                    device if not map_replay_buffer else None,
+                )
+                eval_hist.append(eval_net(config, policy_net, eval_env))
+        # Save new checkpoint
+        save_checkpoint(
+            checkpoint_path(config, checkpoint_iteration).joinpath(name),
+            Checkpoint(
+                num_steps,
+                policy_net.state_dict(),
+                target_net.state_dict(),
+                optimizer.state_dict(),
+                replay_buffer,
+                eval_hist,
+            ),
+        )
+
+        # Communicate checkpoint completion to parent process
+        if mp_barrier is not None:
+            mp_barrier.wait()
+
+
+def init(
+    config: Config,
+    rank: int,
+    device: torch.device,
+) -> None:
+    # All optimizers start with the same initial nets
+    policy_net = QNet(OBS_SHAPE, NUM_ACTIONS).to(device)
+    target_net_state_dict = QNet(OBS_SHAPE, NUM_ACTIONS).state_dict()
+    initial_eval = eval_net(
+        config,
+        policy_net,
+        TorchWrapper(
+            gym.make('CartPole-v1'),
+            device=device,
+            batch_dim=True,
+        ),
+    )
+
+    # Create checkpoints for each optimizer type
+    path = checkpoint_path(config, 0)
+    for optim_rank, optim_config in enumerate(config['optimizers']):
+        optimizer_state_dict = (
+            init_optim(optim_config, policy_net).state_dict()
+        )
+        replay_buffer = deque(maxlen=config['q_replay_buf_len'])
+        save_checkpoint(
+            path.joinpath(checkpoint_name(rank, optim_rank)),
+            Checkpoint(
+                0,
+                policy_net.state_dict(),
+                target_net_state_dict,
+                optimizer_state_dict,
+                replay_buffer,
+                [initial_eval],
+            )
+        )
 
 
 class Args(argparse.Namespace):
@@ -169,6 +406,28 @@ class Args(argparse.Namespace):
     num_iterations: int
     start_iteration: int
     parallel: bool
+    clobber: bool
+    remove_checkpoints: bool
+
+
+def rr_assign(
+    func: typing.Callable,
+    args: list,
+) -> list[mp.Process]:
+    devices = (
+        (torch.device('cuda', i) for i in range(torch.cuda.device_count()))
+        if torch.cuda.is_available()
+        else (torch.device('cpu'),)
+    )
+    processes = []
+    for args_i, device in zip(args, itertools.cycle(devices)):
+        proc = mp.Process(
+            target=func,
+            args=(device, *args_i),
+        )
+        proc.start()
+        processes.append(proc)
+    return processes
 
 
 if __name__ == '__main__':
@@ -187,7 +446,7 @@ if __name__ == '__main__':
         help='Number of training iterations (%(default)s by default).',
     )
     parser.add_argument(
-        '-i',
+        '-s',
         '--start-iteration',
         default=0,
         type=int,
@@ -200,13 +459,89 @@ if __name__ == '__main__':
         action='store_true',
         help='Run deep Q learning algorithms in parallel (multiprocessing) rather than in series. Requires more RAM.'
     )
+    # parser.add_argument(
+    #     '-c',
+    #     '--clobber',
+    #     action='store_true',
+    #     help='Do not check for existing checkpoints. Overwrite them if they exist.',
+    # )
+    parser.add_argument(
+        '-r',
+        '--remove-checkpoints',
+        action='store_true',
+        help='Remove old checkpoints as newer checkpoints are saved.',
+    )
     args = parser.parse_args(namespace=Args)
 
     # Parse config file
+    logger.debug('Parsing config file')
     config = Config(**json.load(args.config_file))
 
-    # Find the latest checkpoint if the start iteration is zero
+    # Find the latest checkpoint if the start iteration is -1
     if args.start_iteration == -1:
+        logger.debug('Starting after most recent checkpoint',
+                     args.start_iteration)
         args.start_iteration = latest_checkpoint_iteration(config) + 1
 
+    # Determine CUDA availability
+    if args.parallel:
+        devices = (
+            (torch.device('cuda', i) for i in range(torch.cuda.device_count()))
+            if torch.cuda.is_available()
+            else (torch.device('cpu'),)
+        )
+    else:
+        device = (
+            torch.device('cuda')
+            if torch.cuda.is_available()
+            else torch.device('cpu')
+        )
+
+    # Initialize if the start iteration is 0
+    if args.start_iteration == 0:
+        args.start_iteration += 1
+        logger.info('Creating initial models for configuration %s',
+                    config['name'])
+        if args.parallel:
+            workers: list[mp.Process] = []
+            for rank, device in zip(
+                range(config['num_models']),
+                itertools.cycle(devices)
+            ):
+                proc = mp.Process(
+                    target=init,
+                    name='init-worker-{}'.format(rank),
+                    args=(config, rank, device),
+                )
+                proc.start()
+                workers.append(proc)
+            for proc in workers:
+                proc.join()
+                proc.close()
+        else:
+            for rank in range(config['num_models']):
+                init(config, rank, device)
+
     # Begin training
+    if args.parallel:
+        pass
+    else:
+        for checkpoint_iteration in range(
+            args.start_iteration,
+            args.start_iteration + args.num_iterations,
+        ):
+            for rank in range(config['num_models']):
+                seed = time.time_ns()
+                for optim_rank in range(len(config['optimizers'])):
+                    deep_q(
+                        config,
+                        rank,
+                        optim_rank,
+                        checkpoint_iteration,
+                        device,
+                        seed,
+                    )
+            if args.remove_checkpoints:
+                prev_path = checkpoint_path(config, checkpoint_iteration - 1)
+                if prev_path.exists():
+                    os.removedirs(prev_path)

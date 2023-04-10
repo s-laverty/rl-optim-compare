1a70f418c054a778ffd157fbc1ad56c472582cb3
Steven <sdlaverty123@gmail.com>
2023-04-10 18:56:51 -0400

    Make smoothing optional

--- visualize.py
+++ visualize.py
@@ -14,9 +14,8 @@ import pathlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-from utils import (Checkpoint, Config, Experience, checkpoint_name,
-                   checkpoint_path, latest_checkpoint_iteration,
-                   load_checkpoint)
+from utils import (Config, checkpoint_name, checkpoint_path,
+                   latest_checkpoint_iteration, load_checkpoint)
 
 logging.basicConfig(
     format='%(asctime)s %(filename)s [%(levelname)s]: %(message)s',
@@ -25,7 +24,7 @@ logger = logging.getLogger(__file__)
 logger.setLevel(logging.DEBUG)
 
 
-def plot_loss(config: Config, iteration: int):
+def plot_loss(config: Config, iteration: int, smoothing: bool):
     path = checkpoint_path(config, iteration)
     if not path.joinpath('COMPLETE').exists():
         raise ValueError('Invalid checkpoint. Checkpoint {} is incomplete'.format(iteration))
@@ -40,12 +39,14 @@ def plot_loss(config: Config, iteration: int):
     mean_loss = loss_history.mean(axis=1)
     confidence_bound_lower = np.quantile(loss_history, 0.25, axis=1)
     confidence_bound_upper = np.quantile(loss_history, 0.75, axis=1)
-    # Get moving averages
-    window_size = 19
-    mean_loss = [np.convolve(x, np.ones(window_size), 'valid') / window_size for x in mean_loss]
-    confidence_bound_lower = [np.convolve(x, np.ones(window_size), 'valid') / window_size for x in confidence_bound_lower]
-    confidence_bound_upper = [np.convolve(x, np.ones(window_size), 'valid') / window_size for x in confidence_bound_upper]
-    iterations = np.arange(window_size // 2, (loss_history.shape[-1] - (window_size - 1) // 2)) * config['train_steps']
+    iterations = np.arange(loss_history.shape[-1]) * config['train_steps']
+    if smoothing:
+        # Get moving averages
+        window_size = 19
+        mean_loss = [np.convolve(x, np.ones(window_size), 'valid') / window_size for x in mean_loss]
+        confidence_bound_lower = [np.convolve(x, np.ones(window_size), 'valid') / window_size for x in confidence_bound_lower]
+        confidence_bound_upper = [np.convolve(x, np.ones(window_size), 'valid') / window_size for x in confidence_bound_upper]
+        iterations = np.arange(window_size // 2, (loss_history.shape[-1] - (window_size - 1) // 2)) * config['train_steps']
     plt.figure(figsize=(7,5))
     for mean_loss_i, lower_i, upper_i, name in zip(
         mean_loss,
@@ -56,7 +57,7 @@ def plot_loss(config: Config, iteration: int):
         plt.fill_between(iterations, lower_i, upper_i, alpha=0.2)
         plt.plot(iterations, mean_loss_i, label=name)
     plt.legend()
-    plt.title('Car-pole performance by optimization algorithm')
+    plt.title('Cart-pole performance by optimization algorithm')
     plt.xlabel('Total steps')
     plt.ylabel('Average duration')
     plt.show()
@@ -66,6 +67,7 @@ if __name__ == '__main__':
     class Args(argparse.Namespace):
         config_file: str
         checkpoint_iteration: int
+        smoothing: bool
     parser = argparse.ArgumentParser()
     parser.add_argument(
         'config_file',
@@ -78,6 +80,12 @@ if __name__ == '__main__':
         default=-1,
         type=int,
         help='Checkpoint iteration to visualize (%(default)s by default). To load the latest checkpoint, use %(metavar)s=-1.',
+    )
+    parser.add_argument(
+        '-s',
+        '--smoothing',
+        action='store_true',
+        help='Use data-smoothing in the plot.',
     )
     args = parser.parse_args(namespace=Args)
 
@@ -92,4 +100,4 @@ if __name__ == '__main__':
         logger.debug('Using most recent checkpoint (iteration %d)', args.checkpoint_iteration)
     
     # Plot the loss history
-    plot_loss(config, args.checkpoint_iteration)
+    plot_loss(config, args.checkpoint_iteration, args.smoothing)

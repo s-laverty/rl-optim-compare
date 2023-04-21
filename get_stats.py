9719ee3a2f948ea4a96a5c698a72c5cf21fa70f1
Steven Laverty <sdlaverty123@gmail.com>
2023-04-21 18:25:23 -0400

    updated checkpoint statistics

--- get_stats.py
+++ get_stats.py
@@ -1,24 +1,40 @@
 import utils
 import json
+import numpy as np
 
 # Parse config file.
 with open('config/perf-results.json') as f:
     config = utils.Config(**json.load(f))
 
-iteration = 1600
+# iteration = 1600
 ranks = (0, 2, 4, 5)
 
 # Get results
 path = utils.checkpoint_path(config, 5)
-episode_lengths = {
-    name:[
-        utils.load_checkpoint(path.joinpath(utils.checkpoint_name(i, rank))) \
-            [5][iteration]
-        for i in range(config['num_models'])
+episode_lengths = np.asarray([
+    [
+        utils.load_checkpoint(path.joinpath(
+            utils.checkpoint_name(rank, optim_rank)))[5]
+        for rank in range(config['num_models'])
     ]
-    for name, rank in zip(
-        (config['optimizers'][rank]['name'] for rank in ranks),
-        ranks,
+    for optim_rank in ranks
+])
+mean_loss = episode_lengths.mean(axis=1)
+best_results = {
+    name: list(zip(best_iterations, best_evals))
+    for name, best_iterations, best_evals in zip(
+        (config['optimizers'][optim_rank]['name'] for optim_rank in ranks),
+        np.argmax(episode_lengths, axis=-1),
+        np.max(episode_lengths, axis=-1),
     )
 }
-print(episode_lengths)
+best_avg_results = {
+    name: (best_iteration, evals[:,best_iteration])
+    for name, best_iteration, evals in zip(
+        (config['optimizers'][optim_rank]['name'] for optim_rank in ranks),
+        np.argmax(mean_loss, axis=-1),
+        episode_lengths,
+    )
+}
+# print(best_results)
+print(best_avg_results)
